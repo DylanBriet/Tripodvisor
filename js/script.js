cc5312bcde92a2e5c7713375e3cb41ff16199662
YannOclock <yann@oclock.io>
2023-11-23 10:24:38 +0100

    gestion du scroll pour affichage newsletter

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const app = {
 
+  newsletterShown: false,
+
   init(){
     app.getAppElements();
     app.addEventListeners();
@@ -12,18 +14,43 @@ const app = {
 
   addEventListeners(){
     app.newsletterLinks.forEach((link) => {
-      link.addEventListener('click', app.showNewsletterFrame);
+      link.addEventListener('click', app.newsletterClickHandler);
     });
-    app.newsletterCloseButton.addEventListener('click', app.hideNewsletterFrame);
+    app.newsletterCloseButton.addEventListener('click', app.newsletterCloseButtonClickHandler);
+    document.addEventListener('scroll', app.scrollHandler);
   },
 
-  showNewsletterFrame(event){
+  newsletterClickHandler(event){
     event.preventDefault();
+    app.showNewsletterFrame();
+  },
+
+  newsletterCloseButtonClickHandler(event){
+    // Le fait de créer une function intermédiaire de gestion d'evenement, permet d'ajouter d'autes action si on le souhaite que de caché l'encart de newsletter
+    app.hideNewsletterFrame();
+    // Si l'utilisateur décide de fermer le popup, on peux désactiver la gestion d'affichage sur scroll, histoire de pas l'énerver
+    document.removeEventListener('scroll', app.scrollHandler);
+  },
+
+  showNewsletterFrame(){
+    console.log('show');
+    app.newsletterShown = true;
     document.querySelector('.newsletter').classList.remove('newsletter--hidden');
   },
 
-  hideNewsletterFrame(event){
+  hideNewsletterFrame(){
+    console.log('hide');
+    app.newsletterShown = false;
     document.querySelector('.newsletter').classList.add('newsletter--hidden');
+  },
+
+  scrollHandler(event){
+    // ! attention gestion de scroll s'exécute énormement a chaque scroll, il est donc conseillé de limiter au maxim le nombre d'exécution d'instruction
+    if(window.scrollY > 300 && !app.newsletterShown){
+      app.showNewsletterFrame();
+    } else if(window.scrollY < 300 && app.newsletterShown) {
+      app.hideNewsletterFrame();
+    }
   }
 
 };

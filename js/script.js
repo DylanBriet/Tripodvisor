96b6360dd9c10830aaa9dba72f5c7aec5b21ed97
YannOclock <yann@oclock.io>
2023-11-23 11:39:42 +0100

    ajout comportement formulaire + dialog

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 const app = {
 
   newsletterShown: false,
+  forbiddenDomains: [
+    '@yopmail.com',
+    '@yopmail.fr',
+    '@yopmail.net',
+    '@cool.fr.nf',
+    '@jetable.fr.nf',
+    '@courriel.fr.nf',
+    '@moncourrier.fr.nf',
+    '@monemail.fr.nf',
+    '@monmail.fr.nf',
+    '@hide.biz.st',
+    '@mymail.infos.st',
+  ],
 
   init(){
     app.getAppElements();
@@ -12,6 +25,11 @@ const app = {
     app.newsletterLinks = document.querySelectorAll('.newsletter-show-link');
     // app.newsletterCloseButton = {Node}
     app.newsletterCloseButton = document.querySelector('.newsletter__close');
+    app.newsletterForm = document.querySelector('.newsletter__form');
+    app.newsletterEmailField = document.querySelector('.newsletter__field');
+    app.dialogError = document.querySelector('.dialog--error');
+    app.dialogClose = document.querySelector('.dialog__close');
+    app.dialogMessage = document.querySelector('.dialog__message');
   },
 
   addEventListeners(){
@@ -34,6 +52,8 @@ const app = {
     });
     app.newsletterCloseButton.addEventListener('click', app.newsletterCloseButtonClickHandler);
     document.addEventListener('scroll', app.scrollHandler);
+    app.newsletterForm.addEventListener('submit', app.newsletterFormSubmitHandler);
+    app.dialogClose.addEventListener('click', app.dialogCloseClickHandler);
   },
 
   newsletterClickHandler(event){
@@ -46,6 +66,34 @@ const app = {
     app.hideNewsletterFrame();
     // Si l'utilisateur décide de fermer le popup, on peux désactiver la gestion d'affichage sur scroll, histoire de pas l'énerver
     document.removeEventListener('scroll', app.scrollHandler);
+  },
+
+  newsletterFormSubmitHandler(event){
+    // On récupère la valeur fourni par l'utilisateur
+    const userEmailInput = app.newsletterEmailField.value;
+    // on vérifie que cette valeur respecte nos règle de validité
+    // On commence par isoler le domaine de l'email
+    // ou faire une recherche sur l'email fourni et vérifier s'il contien un des dommaines interdits
+    // Pour cela on va boucler sur touts les dommaine interdits et faire la vérification a chaque tour de boucle
+
+    // Afin de pouvoir stopper la boucle dès qu'une erreur intervient on utilise une boucle algorithmique que l'on pourra stopper avec "break". Ce que l'on ne peut pas faire avec un forEach
+    for(const forbiddenDomain of app.forbiddenDomains) {
+      console.log(forbiddenDomain);
+      if(userEmailInput.includes(forbiddenDomain)){
+        // on annule la soumission du formulaire
+        event.preventDefault();
+        // alert('Désolé mais cet email est invalide');
+        app.dialogMessage.textContent = 'Désolé mais cet email est invalide';
+        app.dialogError.showModal();
+        break;
+      }
+    }
+  },
+
+  dialogCloseClickHandler(event){
+    // Ce comportement doit fonctionner sur toutes les boityes de dialog, donc il faut récupérer la boite de dialog de manière relative au bouton fermer
+    const closeButton = event.target;
+    closeButton.parentElement.close();
   },
 
   showNewsletterFrame(){
